a7433b6d9e62160f74632f2780f3dcb1a1b478b8
Nick Zadrozny <nick@beyondthepath.com>
2011-12-30 13:51:55 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsolrDemoAdvancedAuth::Application.config.secret_token = '41817feb49bc36156210bce8975b57848c6b025057ac5dbbae3edf9930ac57bf30a99157174a0f2cd637d21d9fe30db602853b7b8121ddd5dda11c40b57c79f9'
